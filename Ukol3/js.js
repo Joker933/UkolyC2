9fe5d80a00261a92e4aa6a52b482f9817972524b
Joker933 <44002024+Joker933@users.noreply.github.com>
2019-09-26 08:34:46 +0200

    Dělo2.

--- Ukol3/js.js
+++ Ukol3/js.js
@@ -3,12 +3,10 @@ window.onload = function() {
   var x = prompt("Zadej Hodnotu X");
   var y = prompt("Zadej Hodnotu Y");
 
-  if(x <= 5){
-    if(y <= 5){
+  var pitagoras = x * x + y * y;
+
+  if(Math.sqrt(pitagoras) > 5){
       document.getElementById("log1").innerHTML="Cíl je v dosahu!";
-    } else {
-      document.getElementById("log2").innerHTML="Cíl není v dosahu!";
-    }
   } else {
     document.getElementById("log2").innerHTML="Cíl není v dosahu!";
   }
