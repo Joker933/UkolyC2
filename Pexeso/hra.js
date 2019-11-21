26b034dbc3caacbd67559542ee7f7fce39f97f86
Joker933 <44002024+Joker933@users.noreply.github.com>
2019-11-21 19:51:43 +0100

    PokrokJakoNikdyJindy
    Kamo tak tohle je třeba nejvetši pokrok.

--- Pexeso/hra.js
+++ Pexeso/hra.js
@@ -1,1 +1,30 @@
-//Ahoj, co zde hledas? Kod zatim zde nehledej :P
+let memory_array = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+
+let array = [...memory_array, ...memory_array];
+
+function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
+
+let party = shuffle(array);
+console.log(party);
+
+window.onload = function() {
+  let nacteni =  document.getElementById('herni_pole');
+
+  for(let i = 0; i < array.length; i++) {
+    let ctverecek = document.createElement('div');
+    ctverecek.innerText = array[i];
+    nacteni.appendChild(ctverecek);
+    ctverecek.classList.add("cteverecek");
+  }
+}
+
+//function klik() {
+//  var x = document.getElementById("cteverecek").textContent;
+//}
+
